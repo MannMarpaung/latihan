31af4e40a017d13cd9b2c72da615fe62e0794fa4
Zaid Hilman <mannmarpaung@gmail.com>
2024-02-13 14:53:51 +0700

    r

--- resources/views/admin/includes/navbar.blade.php
+++ resources/views/admin/includes/navbar.blade.php
@@ -14,7 +14,7 @@
             <input type="text" name="query" placeholder="Search" title="Enter search keyword">
             <button type="submit" title="Search"><i class="bi bi-search"></i></button>
         </form>
-    </div><!-- End Search Barr -->
+    </div><!-- End Search Bar -->
 
     <nav class="header-nav ms-auto">
         <ul class="d-flex align-items-center">
